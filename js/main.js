ab0e8b1e3a2692d98e51f45b8a50d162bea5c90e
Robert De La Torre <robertdelatorre@Roberts-MBP.frontierlocal.net>
2019-07-07 16:30:16 -0700

    Created a robot, method is confidential. Animated ears using CSS.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 $(document).ready(function(){
     const iconOne = $("#gear");
     const menuBar = $(".menu-buttons");
+    const mainBody = $(".body-container");
 
-    $(menuBar).load('./pages/menu.html');
+    $(mainBody).load('./pages/bobot.svg');
+    // $(menuBar).load('./pages/menu.html');
 
 });
