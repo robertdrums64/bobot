40e5474ac80b53c2cfa3781bcd6a6f4a94433d4d
Robert De La Torre <robertdrums64@gmail.com>
2019-07-09 14:59:23 -0700

    Testing

--- js/main.js
+++ js/main.js
@@ -1,9 +1,6 @@
 $(document).ready(function(){
-    const iconOne = $("#gear");
-    const menuBar = $(".menu-buttons");
     const mainBody = $(".body-container");
 
     $(mainBody).load('./pages/bobot.svg');
-    // $(menuBar).load('./pages/menu.html');
 
 });
